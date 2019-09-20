47ea552955a8d232dc4c633330232aec9300aa91
KienLe <kien.le.van.89@gmail.com>
2019-09-20 16:27:42 +0700

    chapter 4

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -5,13 +5,13 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Liquid"
+    assert_select "title", "Liquid"
   end
 
   test "should get home too" do
     get root_url
     assert_response :success
-    assert_select "title", "Home | Liquid"
+    assert_select "title", "Liquid"
   end
 
   test "should get help" do
