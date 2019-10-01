5bbe7907a046f4a97282c64aa1f1cd488adedc7c
KienLe <kien.le.van.89@gmail.com>
2019-10-01 17:41:26 +0700

    Chapter 6 and 7

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup information' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: {
+        name: '', password: '123', password_confirmation: '123', email: 'kien'
+      } }
+    end
+    assert_template 'users/new'
+    assert_select '#error_explanation div ul li', 3
+  end
+
+  test 'valid signup' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: {
+        name: 'shawn', password: '123456',
+        password_confirmation: '123456', email: 'valid@va.lid'
+      } }
+      follow_redirect!
+      assert_template 'users/show'
+      assert_not flash.empty?
+      assert_select '.alert.alert-success', 1
+    end
+  end
+end
